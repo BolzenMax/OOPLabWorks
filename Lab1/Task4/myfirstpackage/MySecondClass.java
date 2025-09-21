95955df33227ddcdf115ecb52fed7073abd4af01
BolzenMax <vorozhmak@gmail.com>
2025-09-21 19:16:12 +0400

    Сделал ЛР-1

---
+++ Lab1/Task4/myfirstpackage/MySecondClass.java
@@ -0,0 +1,31 @@
+package myfirstpackage;
+
+public class MySecondClass {
+	private int Num1;
+	private int Num2;
+	
+	public int getNum1() {
+	    return Num1;
+	}
+	
+	public int getNum2() {
+	    return Num2;
+	}
+	
+	public void setNum1(int val) {
+	    Num1 = val;
+	}
+	
+	public void setNum2(int val) {
+	    Num2 = val;
+	}
+	
+	public MySecondClass(int a, int b) {
+	    Num1 = a;
+	    Num2 = b;
+	}
+	
+	public int bitAnd() {
+	    return Num1 & Num2;
+	}
+}
