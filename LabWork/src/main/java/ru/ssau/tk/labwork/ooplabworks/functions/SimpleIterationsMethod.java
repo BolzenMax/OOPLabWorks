16ad56bfe8712fd6771effca55b3e0abe0b3134d
BolzenMax <vorozhtsovm@outlook.com>
2025-09-28 14:22:30 +0400

    Предварительная реализация метода простых итераций SimpleIterationsMethod

---
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/functions/SimpleIterationsMethod.java
@@ -0,0 +1,34 @@
+package ru.ssau.tk.labwork.ooplabworks.functions;
+
+public class SimpleIterationsMethod implements MathFunction {
+    private final MathFunction g;
+    private final double accuracy;
+    private final int maxIterations;
+
+    public SimpleIterationsMethod (MathFunction g, double accuracy, int maxIterations) {
+        this.g = g;
+        this.accuracy = accuracy;
+        this.maxIterations = maxIterations;
+    }
+
+    public double findRoot(double initial) {
+        double x = initial;
+        int iterations = 0;
+
+        while (iterations < maxIterations) {
+            double nextX = g.apply(x);  // вычисление следующего приближения
+
+            if (Math.abs(nextX - x) < accuracy)
+                return nextX;
+
+            x = nextX;
+            iterations++;
+        }
+
+        return x;
+    }
+
+    public double apply(double x) {
+        return g.apply(x);
+    }
+}
