0ad95a36c01ddf30acdffc8d342b3c481660f397
NIKITAA2000 <someshkin2103@gmail.com>
2025-09-29 12:38:25 +0300

    Расширен интерфейс MathFunction, добавлен CompositeFunction andThen(MathFunction afterFunction)

--- LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/functions/MathFunction.java
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/functions/MathFunction.java
@@ -2,4 +2,8 @@ package ru.ssau.tk.labwork.ooplabworks.functions;
 
 public interface MathFunction {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
 }
