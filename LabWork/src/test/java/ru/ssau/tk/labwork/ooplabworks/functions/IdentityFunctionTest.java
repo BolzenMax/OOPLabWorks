68b5db9b8b615114806a6518296f64ec6db4b615
BolzenMax <vorozhtsovm@outlook.com>
2025-09-29 10:46:45 +0400

    Добавил ещё тестов для IdentityFunction

--- LabWork/src/test/java/ru/ssau/tk/labwork/ooplabworks/functions/IdentityFunctionTest.java
+++ LabWork/src/test/java/ru/ssau/tk/labwork/ooplabworks/functions/IdentityFunctionTest.java
@@ -15,24 +15,40 @@ class IdentityFunctionTest {
     @Test
     void applyPositiveInteger() {
         double res = function.apply(5);
-        Assertions.assertEquals(5,res);
+        double res1 = function.apply(7);
+        double res2 = function.apply(17);
+        Assertions.assertEquals(5, res);
+        Assertions.assertEquals(7, res1);
+        Assertions.assertEquals(17, res2);
     }
 
     @Test
     void applyNegativeInteger() {
         double res = function.apply(-2);
-        Assertions.assertEquals(-2,res);
+        double res1 = function.apply(-10);
+        double res2 = function.apply(-20);
+        Assertions.assertEquals(-2, res);
+        Assertions.assertEquals(-10, res1);
+        Assertions.assertEquals(-20, res2);
     }
 
     @Test
     void applyPositiveDouble() {
         double res = function.apply(0.3);
-        Assertions.assertEquals(0.3,res);
+        double res1 = function.apply(0.0001);
+        double res2 = function.apply(3.1415926);
+        Assertions.assertEquals(0.3, res);
+        Assertions.assertEquals(0.0001, res1);
+        Assertions.assertEquals(3.1415926, res2);
     }
 
     @Test
     void applyNegativeDouble() {
         double res = function.apply(-0.0001);
-        Assertions.assertEquals(-0.0001,res);
+        double res1 = function.apply(-0.17);
+        double res2 = function.apply(-2.718);
+        Assertions.assertEquals(-0.0001, res);
+        Assertions.assertEquals(-0.17, res1);
+        Assertions.assertEquals(-2.718, res2);
     }
 }
