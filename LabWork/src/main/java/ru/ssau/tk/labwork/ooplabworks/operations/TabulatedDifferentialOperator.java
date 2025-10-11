0ec4c6382dfa333358073583ce44cd35b75820a2
BolzenMax <vorozhtsovm@outlook.com>
2025-10-11 14:05:37 +0400

    Предварительно реализовал TabulatedDifferentialOperator. Для окончательной реализации и покрытия тестами жду напарника

---
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/operations/TabulatedDifferentialOperator.java
@@ -0,0 +1,45 @@
+package ru.ssau.tk.labwork.ooplabworks.operations;
+
+import ru.ssau.tk.labwork.ooplabworks.functions.Point;
+import ru.ssau.tk.labwork.ooplabworks.functions.TabulatedFunction;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.TabulatedFunctionFactory;
+
+/*public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction>{
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedDifferentialOperator(TabulatedFunctionFactory factory){
+        this.factory = factory;
+    }
+
+    public TabulatedDifferentialOperator(){
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory(){
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory){
+        this.factory = factory;
+    }
+
+    @Override
+    public TabulatedFunction derive(TabulatedFunction function) {
+        Point[] points = TabulatedFunctionOperationService.asPoints(function);
+        int length = points.length;
+
+        double[] xValues = new double[length];
+        double[] yValues = new double[length];
+
+        for (int i = 0; i < length - 1; ++i) { // n-1 по правой производной
+            xValues[i] = points[i].x;
+            yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
+        }
+
+        xValues[length - 1] = points[length - 1].x; // последнее по левой
+        yValues[length - 1] = (points[length - 1].y - points[length - 2].y) / (points[length - 1].x - points[length - 2].x);
+
+        return factory.create(xValues, yValues);
+    }
+}*/
