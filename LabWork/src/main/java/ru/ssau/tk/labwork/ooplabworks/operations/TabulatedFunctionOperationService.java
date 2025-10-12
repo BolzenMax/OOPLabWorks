ca2b9123c05304cc7cac016e053083401583a905
Никита Горшков <someshkin2103@gmail.com>
2025-10-12 23:57:54 +0300

    Создан класс TabulatedFunctionOperationService, реализован метод Point[] asPoints.

---
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/operations/TabulatedFunctionOperationService.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.labwork.ooplabworks.operations;
+
+import ru.ssau.tk.labwork.ooplabworks.functions.Point;
+import ru.ssau.tk.labwork.ooplabworks.functions.TabulatedFunction;
+
+public class TabulatedFunctionOperationService {
+
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
+        if (tabulatedFunction == null) {
+            return new Point[0];
+        }
+
+        Point[] points = new Point[tabulatedFunction.getCount()];
+        int i = 0;
+        for (Point point : tabulatedFunction) {
+            points[i] = point;
+            i++;
+        }
+        return points;
+    }
+}
