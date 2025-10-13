967262c2d78efe2828608eb9f16d603ac8328e15
BolzenMax <vorozhtsovm@outlook.com>
2025-10-13 14:24:44 +0400

    Реализовал в FunctionsIO writeTabulatedFunction для BufferedOutputStream outstream...

--- LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/io/FunctionsIO.java
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/io/FunctionsIO.java
@@ -1,8 +1,24 @@
 package ru.ssau.tk.labwork.ooplabworks.io;
 
+import ru.ssau.tk.labwork.ooplabworks.functions.Point;
+import ru.ssau.tk.labwork.ooplabworks.functions.TabulatedFunction;
+
+import java.io.*;
+
 public final class FunctionsIO {
 
     private FunctionsIO() {
         throw new UnsupportedOperationException("Error");
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+
+        for (Point point : function){
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
     }
 }
