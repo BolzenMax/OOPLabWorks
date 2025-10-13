82db451d4451be84c1da13654d6e0c88ad184000
Никита Горшков <someshkin2103@gmail.com>
2025-10-13 12:47:22 +0300

    Реализованы методы сложения и вычитания двух функций с помощью BiOperation, методы покрыты тестами

--- LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/operations/TabulatedFunctionOperationService.java
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/operations/TabulatedFunctionOperationService.java
@@ -1,11 +1,31 @@
 package ru.ssau.tk.labwork.ooplabworks.operations;
 
+import ru.ssau.tk.labwork.ooplabworks.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.labwork.ooplabworks.functions.Point;
 import ru.ssau.tk.labwork.ooplabworks.functions.TabulatedFunction;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
 
 
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
 
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         if (tabulatedFunction == null) {
@@ -21,11 +41,47 @@ public class TabulatedFunctionOperationService {
         return points;
     }
 
-    /*public TabulatedFunction multiplication(TabulatedFunction a, TabulatedFunction b) { // умножение
+    @FunctionalInterface
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Функции имеют разное количество точек");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+        int n = pointsA.length;
+
+        double[] xValues = new double[n];
+        double[] yValues = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            if (Math.abs(pointsA[i].x - pointsB[i].x) > 1e-10) {
+                throw new InconsistentFunctionsException("Функции имеют разные значения x в точке " + i);
+            }
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
+
+    public TabulatedFunction multiplication(TabulatedFunction a, TabulatedFunction b) { // умножение
         return doOperation(a, b, (u, v) -> u * v);
     }
 
     public TabulatedFunction division(TabulatedFunction a, TabulatedFunction b) { // деление
         return doOperation(a, b, (u, v) -> u / v);
-    }*/
+    }
 }
--- LabWork/src/test/java/ru/ssau/tk/labwork/ooplabworks/operations/TabulatedFunctionOperationServiceTest.java
+++ LabWork/src/test/java/ru/ssau/tk/labwork/ooplabworks/operations/TabulatedFunctionOperationServiceTest.java
@@ -1,8 +1,14 @@
 package ru.ssau.tk.labwork.ooplabworks.operations;
 
 import org.junit.jupiter.api.Test;
+import ru.ssau.tk.labwork.ooplabworks.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.labwork.ooplabworks.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.labwork.ooplabworks.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.labwork.ooplabworks.functions.Point;
+import ru.ssau.tk.labwork.ooplabworks.functions.TabulatedFunction;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.LinkedListTabulatedFunctionFactory;
+import ru.ssau.tk.labwork.ooplabworks.functions.factory.TabulatedFunctionFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,8 +27,8 @@ class TabulatedFunctionOperationServiceTest {
         assertEquals(10.0, points[0].y, 1e-10);
         assertEquals(2.0, points[1].x, 1e-10);
         assertEquals(20.0, points[1].y, 1e-10);
-        assertEquals(3.0, points[2].x, 1e-10);
-        assertEquals(30.0, points[2].y, 1e-10);
+
+
     }
 
     @Test
@@ -45,4 +51,105 @@ class TabulatedFunctionOperationServiceTest {
         assertEquals(1.0, points[1].y, 1e-10);
     }
 
+    @Test
+    void testAddWithArrayFactory() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new ArrayTabulatedFunctionFactory());
+        TabulatedFunction f1 = new ArrayTabulatedFunction(new double[]{1, 2}, new double[]{10, 20});
+        TabulatedFunction f2 = new ArrayTabulatedFunction(new double[]{1, 2}, new double[]{1, 2});
+
+        TabulatedFunction result = service.add(f1, f2);
+        assertEquals(11, result.getY(0), 1e-10);
+        assertEquals(22, result.getY(1), 1e-10);
+
+    }
+
+    @Test
+    void testSubtractWithLinkedListFactory() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+        TabulatedFunction f1 = new LinkedListTabulatedFunction(new double[]{1, 2}, new double[]{10, 20});
+        TabulatedFunction f2 = new LinkedListTabulatedFunction(new double[]{1, 2}, new double[]{3, 4});
+
+        TabulatedFunction result = service.subtract(f1, f2);
+        assertEquals(7, result.getY(0), 1e-10);
+        assertEquals(16, result.getY(1), 1e-10);
+
+    }
+
+    @Test
+    void testAddDifferentTypes() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new ArrayTabulatedFunctionFactory());
+        TabulatedFunction f1 = new ArrayTabulatedFunction(new double[]{1, 2}, new double[]{10, 20});
+        TabulatedFunction f2 = new LinkedListTabulatedFunction(new double[]{1, 2}, new double[]{1, 2});
+
+        TabulatedFunction result = service.add(f1, f2);
+        assertEquals(11, result.getY(0), 1e-10);
+        assertEquals(22, result.getY(1), 1e-10);
+
+    }
+
+    @Test
+    void testSubtractDifferentTypes() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+        TabulatedFunction f1 = new LinkedListTabulatedFunction(new double[]{1, 2}, new double[]{10, 20});
+        TabulatedFunction f2 = new ArrayTabulatedFunction(new double[]{1, 2}, new double[]{3, 4});
+
+        TabulatedFunction result = service.subtract(f1, f2);
+        assertEquals(7, result.getY(0), 1e-10);
+        assertEquals(16, result.getY(1), 1e-10);
+
+    }
+    @Test
+    void testMultiplicationDifferentTypes() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new ArrayTabulatedFunctionFactory());
+        TabulatedFunction f1 = new ArrayTabulatedFunction(new double[]{1, 2}, new double[]{3, 4});
+        TabulatedFunction f2 = new LinkedListTabulatedFunction(new double[]{1, 2}, new double[]{5, 6});
+
+        TabulatedFunction result = service.multiplication(f1, f2);
+        assertEquals(15, result.getY(0), 1e-10);
+        assertEquals(24, result.getY(1), 1e-10);
+    }
+
+    @Test
+    void testDivisionDifferentTypes() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+        TabulatedFunction f1 = new LinkedListTabulatedFunction(new double[]{2, 4}, new double[]{10, 20});
+        TabulatedFunction f2 = new ArrayTabulatedFunction(new double[]{2, 4}, new double[]{2, 5});
+
+        TabulatedFunction result = service.division(f1, f2);
+        assertEquals(5, result.getY(0), 1e-10);
+        assertEquals(4, result.getY(1), 1e-10);
+
+    }
+    @Test
+    void testDifferentCountsThrowsException() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+        TabulatedFunction a = new ArrayTabulatedFunction(new double[]{1, 2}, new double[]{10, 20});
+        TabulatedFunction b = new ArrayTabulatedFunction(new double[]{1, 2, 3}, new double[]{1, 2, 3});
+
+        assertThrows(InconsistentFunctionsException.class, () -> service.add(a, b));
+    }
+
+    @Test
+    void testGetAndSetFactory() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunctionFactory newFactory = new LinkedListTabulatedFunctionFactory();
+        service.setFactory(newFactory);
+        assertSame(newFactory, service.getFactory());
+    }
+
+    @Test
+    void testInconsistentXValuesThrowException() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+        TabulatedFunction a = new ArrayTabulatedFunction(new double[]{1.0, 2.0}, new double[]{10, 20});
+        TabulatedFunction b = new ArrayTabulatedFunction(new double[]{1.0, 3}, new double[]{1, 2});
+
+        assertThrows(InconsistentFunctionsException.class, () -> service.add(a, b));
+    }
+
+    @Test
+    void testDefaultConstructorUsesArrayFactory() {
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+    }
 }
