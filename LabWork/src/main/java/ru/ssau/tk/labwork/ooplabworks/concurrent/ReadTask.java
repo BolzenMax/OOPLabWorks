055a04e904bea826fb21c04a7f3fca1ee5abf18d
BolzenMax <vorozhtsovm@outlook.com>
2025-10-25 15:10:15 +0400

    Реализован класс ReadTask в пакете concurrent

---
+++ LabWork/src/main/java/ru/ssau/tk/labwork/ooplabworks/concurrent/ReadTask.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.labwork.ooplabworks.concurrent;
+
+import ru.ssau.tk.labwork.ooplabworks.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable {
+
+    private final TabulatedFunction function;
+
+    public ReadTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            synchronized (function) {
+                double x = function.getX(i);
+                double y = function.getY(i);
+                System.out.printf("After read: i = %d, x = %f, y = %f\n", i, x, y);
+                System.out.flush();
+            }
+        }
+    }
+}
